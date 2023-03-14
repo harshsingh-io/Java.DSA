fde4a29103d6c25a1bbcd4c1f038a1fc4e45f1b8
Harsh Singh <89487399+harshsingh-io@users.noreply.github.com>
2023-03-15 01:37:21 +0530

    Source Code Added Till Basics Of Dynamic Programming

---
+++ src/DynamicProgrammingB.java
@@ -0,0 +1,58 @@
+import java.util.Arrays;
+public class DynamicProgrammingB {
+    public static int climbingStairsWays(int n) {
+        //recursion
+        if (n == 0) {
+            return 1;
+        }
+        if (n < 0) {
+            return 0;
+        }
+        return climbingStairsWays(n - 1) + climbingStairsWays(n - 2);
+    }
+
+    public static int climbingStairsWays(int n, int[] ways) {
+        //recursion
+        if (n == 0) {
+            return 1;
+        }
+        if (n < 0) {
+            return 0;
+        }
+        if (ways[n] != -1) {
+            return ways[n];
+        }
+        ways[n] = climbingStairsWays(n - 1) + climbingStairsWays(n - 2);
+        return ways[n];
+    }
+
+    public static int fib(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public static int climbingStairs(int n) {
+        int[] ways = new int[n + 1];
+        ways[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            if (i == 1) {
+                ways[i] = ways[i - 1] + 0;
+            } else {
+                ways[i] = ways[i - 1] + ways[i - 2];
+            }
+        }
+        return ways[n];
+    }
+    
+
+    public static void main(String[] args) {
+        int n = 5;
+        int[] ways = new int[n + 1];
+        Arrays.fill(ways, -1);
+        System.out.println(climbingStairsWays(n));
+//        System.out.println(climbingStairsWays(n,ways));
+        System.out.println(climbingStairsWays(n));
+    }
+}
---
+++ src/UniqueSubstring.java
@@ -0,0 +1,73 @@
+public class UniqueSubstring {
+    static class Node {
+        Node[] children = new Node[26];
+        boolean eow = false;
+
+        Node() {
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+        }
+    }
+
+    public static void insert(String word) { //O(L) where L is length of largest word
+        Node curr = root;
+        for (int level = 0; level < word.length(); level++) {
+            int idx = word.charAt(level) - 'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+        curr.eow = true;
+    }
+
+    public static boolean search(String word) { //O(L)
+        Node curr = root;
+        for (int level = 0; level < word.length(); level++) {
+            int idx = word.charAt(level) - 'a';
+            if (curr.children[idx] == null) {
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return curr.eow;
+    }
+
+    public static boolean startsWith(String prefix) { //O(L) where l is the length of prefix
+        Node curr = root;
+        for (int level = 0; level < prefix.length(); level++) {
+            int idx = prefix.charAt(level) - 'a';
+            if (curr.children[idx] == null) {
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return true;
+    }
+
+    public static int countNodes(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int count = 0;
+        for (int i = 0; i < 26; i++) {
+            if (root.children[i] != null) {
+                count += countNodes(root.children[i]);
+            }
+        }
+        return count + 1;
+    }
+
+    public static Node root = new Node();
+
+    public static void main(String[] args) {
+        String str = "ababa"; //ans = 10
+        for (int i = 0; i < str.length(); i++) {
+            String suffix = str.substring(i);
+            insert(suffix);
+        }
+        System.out.println(countNodes(root));
+
+    }
+}
