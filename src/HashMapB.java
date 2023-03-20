f11f51b759150463adda5ab89effc6312ce7f9e9
Harsh Singh <89487399+harshsingh-io@users.noreply.github.com>
2023-03-21 00:13:55 +0530

    HashMap Operation Code Added

--- src/HashMapB.java
+++ src/HashMapB.java
@@ -3,6 +3,7 @@ import java.util.Set;
 
 public class HashMapB {
     public static void main(String[] args) {
+        //Create
         HashMap<String, Integer> hashMap = new HashMap<>();
 
         //Insert - O(1)
@@ -10,21 +11,21 @@ public class HashMapB {
         hashMap.put("China", 150);
         hashMap.put("US", 50);
 //        System.out.println(hashMap);
-//
+
 //        //Get - O(1)
 //        int population = hashMap.get("India");
-//        System.out.println(population);
-//        System.out.println(hashMap.get("Indonesia"));
-//
+//        System.out.println(population); // 100 returned
+//        System.out.println(hashMap.get("Indonesia")); //null return
+
 //        //ContainKeus - O(1)
 //        System.out.println(hashMap.containsKey("India")); //true
-//        System.out.println(hashMap.containsKey("Indonesia"));
+//        System.out.println(hashMap.containsKey("Indonesia")); //false
 
         //Remove - O(1)
 //        System.out.println(hashMap.remove("China"));
 //        System.out.println(hashMap);
 //        System.out.println(hashMap.remove("Indonesia"));
-//
+
 //        //Size O(1)
 //        System.out.println(hashMap.size());
 
@@ -41,8 +42,7 @@ public class HashMapB {
         Set<String> keys = hashMap.keySet();
         System.out.println(keys);
 
-        for (String k : keys
-             ) {
+        for (String k : keys) {
             System.out.println("Key = "+k+ ", Value = "+hashMap.get(k));
         }
     }
