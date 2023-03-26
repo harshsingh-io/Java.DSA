34736bc864c514697cb45d768b0f8276c048e54c
Harsh Singh <89487399+harshsingh-io@users.noreply.github.com>
2023-03-27 03:30:22 +0530

    Code till Itinerary Tickets Problem

--- src/ItineraryTickets.java
+++ src/ItineraryTickets.java
@@ -1,5 +1,30 @@
-public class ItineraryTickets { //journey(itinerary)
-    public static void main(String[] args) {
+import java.util.HashMap;
 
+public class ItineraryTickets { //journey(itinerary)
+    public static String getStart(HashMap<String, String> tickets){
+        HashMap<String, String> revMap = new HashMap<>();
+        for (String key:tickets.keySet()){
+            revMap.put(tickets.get(key),key);
+        }
+        for (String key:tickets.keySet()){
+            if (!revMap.containsKey(key)){
+                return key;
+            }
+        }
+        return null;
+    }
+    public static void main(String[] args) {
+        HashMap<String,String> tickets = new HashMap<>();
+        tickets.put("Chennai","Bengaluru");
+        tickets.put("Mumbai","Delhi");
+        tickets.put("Goa","Chennai");
+        tickets.put("Delhi","Goa");
+        String start = getStart(tickets);
+        System.out.print(start);
+        for (String key: tickets.keySet()){
+            System.out.print(" -> " + tickets.get(start));
+            start = tickets.get(start);
+        }
+        System.out.println();
     }
 }
