8e79bd844696b000b4efb0f1b5fef8af16a776b9
Harsh Singh <89487399+harshsingh-io@users.noreply.github.com>
2023-04-14 17:25:44 +0530

    Code till Longest Common Subsequences

--- src/LongestCommonSubsequences.java
+++ src/LongestCommonSubsequences.java
@@ -26,6 +26,30 @@ public class LongestCommonSubsequences {
             return dp[n][m]= Math.max(lcs1, lcs2);
         }
     }
+    public static int lcsTab(String str1, String str2){
+        int n = str1.length();
+        int m = str2.length();
+        int dp[][] = new int[n+1][m+1];
+        for (int i=0; i<n+1;i++){
+            for(int j=0;j<m+1;j++){
+                if (i==0||j==0){
+                    dp[i][j]=0;
+                }
+            }
+        }
+        for (int i=1; i<n+1;i++) {
+            for (int j =1; j < m + 1; j++) {
+                if (str1.charAt(i-1)==str2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }else {
+                    int ans1 = dp[i-1][j];
+                    int ans2 = dp[i][j-1];
+                    dp[i][j]=Math.max(ans1,ans2);
+                }
+            }
+        }
+        return dp[n][m];
+    }
 
     public static void main(String[] args) {
         String str1 = "abcdge";
@@ -33,13 +57,14 @@ public class LongestCommonSubsequences {
 //        System.out.println(lcs(str1,str2,str1.length(),str2.length()));
         int n = str1.length();
         int m = str2.length();
-        int[][] dp = new int[n+1][m+1];
-        //initialization
-        for (int i=0;i<n+1;i++){
-            for (int j=0;j<m+1;j++){
-                dp[i][j] = -1;
-            }
-        }
-        System.out.println(lcsMemoization(str1,str2,n,m,dp));
+//        int[][] dp = new int[n+1][m+1];
+//        //initialization
+//        for (int i=0;i<n+1;i++){
+//            for (int j=0;j<m+1;j++){
+//                dp[i][j] = -1;
+//            }
+//        }
+//        System.out.println(lcsMemoization(str1,str2,n,m,dp));
+        System.out.println(lcsTab(str1,str2));
     }
 }
