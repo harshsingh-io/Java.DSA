e461037704c6934ae2fb8da881c79c655139a94e
Harsh Singh <89487399+harshsingh-io@users.noreply.github.com>
2023-04-21 01:22:32 +0530

    Code till Counting BST Problem

---
+++ src/CountingBST.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class CountingBST {
+    //refer to catalan's Number problem firstly
+    public static int countingBST(int n){
+        int [] dp = new int[n+1];
+        dp[0]=dp[1]=1;
+        for (int i=2;i<=n;i++){
+            //Ci => BST (i nodes) -> dp[i]
+            for (int j=0;j<i;j++){
+                int left = dp[j]; // for left subtree
+                int right = dp[i-j-1]; // for right subtree
+                dp[i]+=left*right; //Ci= Cj * Ci-j-1
+
+            }
+        }
+        return dp[n];
+    }
+    public static void main(String[] args) {
+        int n=4;
+        System.out.println(countingBST(n));
+    }
+}
