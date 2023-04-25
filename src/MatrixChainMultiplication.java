ec789918199421c55835197bb4f95706387f856c
Harsh Singh <89487399+harshsingh-io@users.noreply.github.com>
2023-04-25 14:27:39 +0530

    Code till Matrix chain multiplication Memoization Approach

--- src/MatrixChainMultiplication.java
+++ src/MatrixChainMultiplication.java
@@ -1,21 +1,46 @@
 import java.util.*;
+
 public class MatrixChainMultiplication {
-    public static int mcm(int[] arr, int i, int j){
-        if (i==j) return 0; //single matrix cost/case
-        int ans= Integer.MAX_VALUE;
-        for (int k=i;k<j;k++){
-            int cost1=mcm(arr,i,k); //Ai...Ak => arr[i-1}*arr[k]
-            int cost2=mcm(arr,k+1,j); //Ai+1...Aj => arr[k]*arr[j]
-            int cost3= arr[i-1]*arr[k]*arr[j];
-            int finalCost = cost1+cost2+cost3;
-            ans = Math.min(ans,finalCost);
+    public static int mcm(int[] arr, int i, int j) {
+        if (i == j) return 0; //single matrix cost/case
+        int ans = Integer.MAX_VALUE;
+        for (int k = i; k < j; k++) {
+            int cost1 = mcm(arr, i, k); //Ai...Ak => arr[i-1}*arr[k]
+            int cost2 = mcm(arr, k + 1, j); //Ai+1...Aj => arr[k]*arr[j]
+            int cost3 = arr[i - 1] * arr[k] * arr[j];
+            int finalCost = cost1 + cost2 + cost3;
+            ans = Math.min(ans, finalCost);
         }
         return ans;
     }
+
+    public static int mcmMemoization(int[] arr, int[][] dp, int i, int j) {
+        if (i == j) return 0; //single matrix cost/case
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        int ans = Integer.MAX_VALUE;
+        for (int k = i; k < j; k++) {
+            int cost1 = mcm(arr, i, k); //Ai...Ak => arr[i-1}*arr[k]
+            int cost2 = mcm(arr, k + 1, j); //Ai+1...Aj => arr[k]*arr[j]
+            int cost3 = arr[i - 1] * arr[k] * arr[j];
+            int finalCost = cost1 + cost2 + cost3;
+            ans = Math.min(ans, finalCost);
+        }
+        return dp[i][j] = ans;
+    }
+
     public static void main(String[] args) {
-        int[] arr={1,2,3,4,3};
-        int n=arr.length;
-        System.out.println(mcm(arr,1,n-1));
+        int[] arr = {1, 2, 3, 4, 3};
+        int n = arr.length;
+        System.out.println(mcm(arr, 1, n - 1));
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        System.out.println(mcmMemoization(arr, dp, 1, n - 1));
 
     }
 }
