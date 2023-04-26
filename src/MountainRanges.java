4d8833d900fccbf6f0465282c0624be42b38db76
Harsh Singh <89487399+harshsingh-io@users.noreply.github.com>
2023-04-27 00:59:27 +0530

    Code till Matrix Chain Multiplication Tabulation Approach

--- src/MountainRanges.java
+++ src/MountainRanges.java
@@ -1,17 +1,18 @@
 public class MountainRanges {
-    public static int mountainRanges(int n){
-        int [] dp = new int[n+1];
-        dp[0]=dp[1]=1;
-        for (int i=2;i<=n;i++){
+    public static int mountainRanges(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
             //Ci => i Up and Down Range Pair ->  Mountain Range == dp[i]
-            for (int j=0;j<i;j++){
+            for (int j = 0; j < i; j++) {
                 int inside = dp[j]; // for Inside arrangement possible cases
-                int outside = dp[i-j-1]; // for Outside arrangement possible cases
-                dp[i]+=inside*outside; //Ci= Cj * Ci-j-1
+                int outside = dp[i - j - 1]; // for Outside arrangement possible cases
+                dp[i] += inside * outside; //Ci= Cj * Ci-j-1
             }
         }
         return dp[n];
     }
+
     public static void main(String[] args) {
         int pair = 3;
         System.out.println(mountainRanges(pair));
