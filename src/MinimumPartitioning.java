a4e59d6827a9d8162b7b094aae017cbe35bdf69b
Harsh Singh <89487399+harshsingh-io@users.noreply.github.com>
2023-04-28 23:50:39 +0530

    Code till Minimum Partitioning Problem

--- src/MinimumPartitioning.java
+++ src/MinimumPartitioning.java
@@ -2,10 +2,10 @@ public class MinimumPartitioning {
     public static int minPartition(int[] arr) { // 0-1 Knapsack Code
         int n = arr.length;
         int sum = 0;
-        for (int i=0;i<arr.length;i++){
-            sum+= arr[i];
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
         }
-        int W = sum/2;
+        int W = sum / 2;
         int[][] dp = new int[n + 1][W + 1];
         for (int i = 0; i < dp.length; i++) { // 0th Column
             dp[i][0] = 0;
@@ -16,9 +16,9 @@ public class MinimumPartitioning {
         //Tabulation LOOP
         for (int i = 1; i < n + 1; i++) {
             for (int j = 1; j < W + 1; j++) {
-                if (arr[i-1] <= j) { //Valid Condition
-                    int includeProfit = arr[i-1]+dp[i-1][j-arr[i-1]];
-                    int excludeProfit = dp[i-1][j];
+                if (arr[i - 1] <= j) { //Valid Condition
+                    int includeProfit = arr[i - 1] + dp[i - 1][j - arr[i - 1]];
+                    int excludeProfit = dp[i - 1][j];
                     dp[i][j] = Math.max(includeProfit, excludeProfit);
                 } else { //Invalid Condition
                     int excludeProfit = dp[i - 1][j];
@@ -26,13 +26,13 @@ public class MinimumPartitioning {
                 }
             }
         }
-        int sum1= dp[n][W];
-        int sum2 = sum-sum1;
-        return Math.abs(sum1-sum2);
+        int sum1 = dp[n][W];
+        int sum2 = sum - sum1;
+        return Math.abs(sum1 - sum2);
     }
 
     public static void main(String[] args) {
-        int[] num ={1, 6, 11, 5};
+        int[] num = {1, 6, 11, 5};
         System.out.println(minPartition(num));
     }
 }
