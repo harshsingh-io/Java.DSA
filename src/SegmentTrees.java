7304fc2d0c28fc564cf04583f2e176ee58e011c4
Harsh Singh <89487399+harshsingh-io@users.noreply.github.com>
2023-05-04 01:22:33 +0530

    Code till Query in Segment Tree

--- src/SegmentTrees.java
+++ src/SegmentTrees.java
@@ -16,22 +16,25 @@ public class SegmentTrees {
         tree[i] = tree[2 * i + 1] + tree[2 * i + 2];
         return tree[i];
     }
+
     public static int getSumUtil(int i, int si, int sj, int qi, int qj) {
-        if (qj<=si||qi>=sj){ //non overlapping
+        if (qj <= si || qi >= sj) { //non overlapping
             return 0;
-        } else if (si>=qi && sj<=qj) { //complete overlap
+        } else if (si >= qi && sj <= qj) { //complete overlap
             return tree[i];
         } else { //partial overlap
-            int mid = (si+sj)/2;
-            int left = getSumUtil(2*i+1,si,mid,qi,qj);
-            int right = getSumUtil(2*i+2,mid+1,sj,qi,qj);
-            return left+right;
+            int mid = (si + sj) / 2;
+            int left = getSumUtil(2 * i + 1, si, mid, qi, qj);
+            int right = getSumUtil(2 * i + 2, mid + 1, sj, qi, qj);
+            return left + right;
         }
     }
-    public static int getSum(int[] arr,int qi,int qj){
+
+    public static int getSum(int[] arr, int qi, int qj) {
         int n = arr.length;
-        return getSumUtil(0,0,n-1,qi,qj);
+        return getSumUtil(0, 0, n - 1, qi, qj);
     }
+
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
         int n = arr.length;
@@ -40,6 +43,6 @@ public class SegmentTrees {
 //        for (int i = 0; i < tree.length; i++) {
 //            System.out.print(tree[i] + " ");
 //        }
-        System.out.println(getSum(arr,2,5));
+        System.out.println(getSum(arr, 2, 5));
     }
 }
