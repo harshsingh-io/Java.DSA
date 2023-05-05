c75a77307bf408a53cbd4f448d9aaaa33c0dc0be
Harsh Singh <89487399+harshsingh-io@users.noreply.github.com>
2023-05-05 19:37:48 +0530

    Code till Update in Segment Tree

--- src/SegmentTrees.java
+++ src/SegmentTrees.java
@@ -35,6 +35,26 @@ public class SegmentTrees {
         return getSumUtil(0, 0, n - 1, qi, qj);
     }
 
+    public static void updateUtil(int i, int si, int sj, int idx, int diff) {
+        if (idx > sj || idx < si) {
+            return;
+        }
+        tree[i] += diff;
+        if (si != sj) { //non-leaf
+            int mid = (si + sj) / 2;
+            updateUtil(2 * i + 1, si, mid, idx, diff);
+            updateUtil(2 * i + 2, mid + 1, sj, idx, diff);
+        }
+    }
+
+    public static void update(int[] arr, int idx, int newVal) {
+        int n = arr.length;
+        int diff = newVal - arr[idx];
+        arr[idx] = newVal;
+
+        updateUtil(0, 0, n - 1, idx, diff);
+    }
+
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
         int n = arr.length;
@@ -44,5 +64,7 @@ public class SegmentTrees {
 //            System.out.print(tree[i] + " ");
 //        }
         System.out.println(getSum(arr, 2, 5));
+        update(arr,2,2);
+        System.out.println(getSum(arr,2,5));
     }
 }
