38da02cdade926603aebe7b72dd254af3e3837f9
Harsh Singh <89487399+harshsingh-io@users.noreply.github.com>
2023-05-07 02:00:01 +0530

    Code till MaxElementQueries Problem(Update)

--- src/MaxElementQuery.java
+++ src/MaxElementQuery.java
@@ -16,7 +16,7 @@ public class MaxElementQuery {
         tree[i] = Math.max(tree[2 * i + 1],tree[2 * i + 2]);
     }
     public static int getMaxUtil(int i, int si, int sj, int qi, int qj) {
-        if (qj <= si || qi >= sj) { //non overlapping
+        if (qj < si || qi > sj) { //non overlapping
             return Integer.MIN_VALUE;
         } else if (si >= qi && sj <= qj) { //complete overlap
             return tree[i];
@@ -32,6 +32,24 @@ public class MaxElementQuery {
         int n = arr.length;
         return getMaxUtil(0, 0, n - 1, qi, qj);
     }
+    public static void updateUtil(int i, int si, int sj, int idx, int newVal) {
+        if (idx > sj || idx < si) {
+            return;
+        }
+        tree[i] = Math.max(tree[i],newVal);
+        if (si != sj) { //non-leaf
+            int mid = (si + sj) / 2;
+            updateUtil(2 * i + 1, si, mid, idx, newVal);
+            updateUtil(2 * i + 2, mid + 1, sj, idx, newVal);
+        }
+    }
+
+    public static void update(int[] arr, int idx, int newVal) {
+        arr[idx] = newVal;
+        int n = arr.length;
+        updateUtil(0, 0, n - 1, idx, newVal);
+    }
+
     public static void main(String[] args) {
         int[] arr = {6,8,-1,2,17,1,3,2,4};
         init(arr.length);
@@ -39,6 +57,8 @@ public class MaxElementQuery {
 //        for (int i=0;i<tree.length;i++){
 //            System.out.print(tree[i]+" ");
 //        }
+        System.out.println(getMax(arr,2,5));
+        update(arr,2,20);
         System.out.println(getMax(arr,2,5));
     }
 }
