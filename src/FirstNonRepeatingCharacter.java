aa067e7d592ebe475f37c06fcddfc16f8b124e14
Harsh Singh <89487399+harshsingh-io@users.noreply.github.com>
2023-05-16 23:44:29 +0530

    Formatted the code of First Non-Repeating Character Problem

--- src/FirstNonRepeatingCharacter.java
+++ src/FirstNonRepeatingCharacter.java
@@ -2,24 +2,25 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class FirstNonRepeatingCharacter {
-    public static void printNonRepeating(String str){
+    public static void printNonRepeating(String str) {
         int[] freq = new int[26]; // 'a' to 'z'
         Queue<Character> q = new LinkedList<>();
-        for (int i=0;i<str.length();i++){
+        for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
             q.add(ch);
-            freq[ch-'a']++;
-            while (!q.isEmpty() && freq[q.peek()-'a']>1){
+            freq[ch - 'a']++;
+            while (!q.isEmpty() && freq[q.peek() - 'a'] > 1) {
                 q.remove();
             }
-            if (q.isEmpty()){
-                System.out.print(-1+" ");
+            if (q.isEmpty()) {
+                System.out.print(-1 + " ");
             } else {
-                System.out.print(q.peek()+" ");
+                System.out.print(q.peek() + " ");
             }
         }
         System.out.println();
     }
+
     public static void main(String[] args) {
         String str = "aabccxb";
         printNonRepeating(str);
