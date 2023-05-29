37f07af35a6cd162bb504a9016165e2d5e90ea88
Harsh Singh <89487399+harshsingh-io@users.noreply.github.com>
2023-05-29 23:52:28 +0530

    Code till Grid Ways Problem

--- src/BackTracking.java
+++ src/BackTracking.java
@@ -1,8 +1,8 @@
 public class BackTracking {
-    public static void findSubString(String str, String ans, int i){
+    public static void findSubString(String str, String ans, int i) {
 //        Base Case
-        if (i==str.length()){
-            if (ans.length()==0){
+        if (i == str.length()) {
+            if (ans.length() == 0) {
                 System.out.print("null");
             }
             System.out.print(ans + " ");
@@ -10,27 +10,29 @@ public class BackTracking {
         }
 //        Recursive call
 //        if yes choice
-        findSubString(str,ans+str.charAt(i),i+1);
+        findSubString(str, ans + str.charAt(i), i + 1);
 //        if no choice
-        findSubString(str,ans,i+1);
+        findSubString(str, ans, i + 1);
     }
-    public static void findPermutation(String str, String ans){
+
+    public static void findPermutation(String str, String ans) {
 //        Base Case
-        if (str.length()==0){
+        if (str.length() == 0) {
             System.out.println(ans);
             return;
         }
 //        recursion
-        for (int i = 0; i < str.length(); i++){
+        for (int i = 0; i < str.length(); i++) {
             char cur = str.charAt(i);
 //            "abcde" => "ab"+"de" = "abde"
-            String NewStr = str.substring(0,i)+str.substring(i+1);
-            findPermutation(NewStr,ans+cur);
+            String NewStr = str.substring(0, i) + str.substring(i + 1);
+            findPermutation(NewStr, ans + cur);
         }
     }
-    public static void nQueens(char[][] board, int row){
+
+    public static void nQueens(char[][] board, int row) {
 //        Base Condition
-        if (row==board.length){
+        if (row == board.length) {
 //            to print board
 //            printBoard(board);
 //            to count total solution
@@ -38,17 +40,18 @@ public class BackTracking {
             return;
         }
 //        column loop
-        for (int j = 0; j<board.length;j++){
-            if (isSafe(board,row,j)) {
+        for (int j = 0; j < board.length; j++) {
+            if (isSafe(board, row, j)) {
                 board[row][j] = 'Q';
                 nQueens(board, row + 1);//Function Call
                 board[row][j] = 'X';//Back Tracking
             }
         }
     }
-    public static boolean CheckNQueens(char[][] board, int row){
+
+    public static boolean CheckNQueens(char[][] board, int row) {
 //        Base Condition
-        if (row==board.length){
+        if (row == board.length) {
 //            to print board
 //            printBoard(board);
 //            to count total solution
@@ -56,10 +59,10 @@ public class BackTracking {
             return true;
         }
 //        column loop
-        for (int j = 0; j<board.length;j++){
-            if (isSafe(board,row,j)) {
+        for (int j = 0; j < board.length; j++) {
+            if (isSafe(board, row, j)) {
                 board[row][j] = 'Q';
-                if(CheckNQueens(board, row + 1)){
+                if (CheckNQueens(board, row + 1)) {
                     return true;
                 }
                 board[row][j] = 'X';//Back Tracking
@@ -67,44 +70,65 @@ public class BackTracking {
         }
         return false;
     }
+
     static int count = 0;
-    public static boolean isSafe(char[][] board, int row, int col){
+
+    public static boolean isSafe(char[][] board, int row, int col) {
 //        vertically up
-        for (int i = row-1;i>=0;i--){
-            if (board[i][col]=='Q'){
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q') {
                 return false;
             }
         }
 //        Diagonal left up
-        for (int i = row-1,j=col-1;i>=0&&j>=0;i--,j--){
-            if (board[i][j]=='Q'){
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
                 return false;
             }
         }
 //        Diagonal right up
-        for (int i = row-1,j=col+1;i>=0&&j< board.length;i--,j++){
-            if (board[i][j]=='Q') {
+        for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
                 return false;
             }
         }
-        return  true;
+        return true;
     }
-    public static void printBoard(char[][] board){
+
+    public static void printBoard(char[][] board) {
         System.out.println("------CHESS BOARD------");
-        for (int i = 0; i <board.length;i++){
-            for (int j = 0;j<board.length;j++){
-                System.out.print(board[i][j]+" ");
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                System.out.print(board[i][j] + " ");
             }
             System.out.println();
         }
     }
+
+    public static int gridWays(int i, int j, int n, int m) {
+        //base case
+        if (i == n - 1 && j == m - 1) { // condition for last cell
+            return 1;
+        } else if (i == n || j == n) { //boundary cross condition
+            return 0;
+        }
+
+        int w1 = gridWays(i + 1, j, n, m);
+        int w2 = gridWays(i, j + 1, n, m);
+        return w1 + w2;
+    }
+
     public static void main(String[] args) {
 //        String str = "abc";
 //        findSubString(str,"",0);
 //        findPermutation(str,"");
-        int n = 4;
-        char[][] chess = new char[n][n];
-        nQueens(chess, 0);
-        System.out.println("Total ways to solve nQueens = " + count);
+//        int n = 4;
+//        char[][] chess = new char[n][n];
+//        nQueens(chess, 0);
+//        System.out.println("Total ways to solve nQueens = " + count);
+        
+        int n = 3, m = 3;
+        System.out.println(gridWays(0,0,n,m));
+
     }
 }
