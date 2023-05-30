ed0446ce964695c37128c7f38fdcb34506a8e3cb
Harsh Singh <89487399+harshsingh-io@users.noreply.github.com>
2023-05-30 14:42:53 +0530

    Code till Target Sum Subset Problem(Tabulation)

---
+++ src/TargetSum.java
@@ -0,0 +1,27 @@
+public class TargetSum {
+    public static boolean targetSumSubset(int[] arr, int sum){
+        int n = arr.length;
+        boolean[][] dp = new boolean[n+1][sum+1];
+        //i = items & j = target sum
+        for (int i=0;i<n+1;i++){
+            dp[i][0] = true;
+        }
+        for (int i=1; i<n+1;i++) {
+            for(int j=1; j<sum+1;j++){
+                int v=arr[i-1];
+                //include
+                if (v<=j && dp[i-1][j-v] == true){
+                    dp[i][j] = true;
+                }
+                //exclude
+                else if (dp[i - 1][j] == true) {
+                    dp[i][j] = true;
+                }
+            }
+        }
+        return dp[n][sum];
+    }
+    public static void main(String[] args) {
+
+    }
+}
