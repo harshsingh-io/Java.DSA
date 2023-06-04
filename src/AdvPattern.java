dbf2095975db55023a562efb62811becdbe94c02
Harsh Singh <89487399+harshsingh-io@users.noreply.github.com>
2023-06-04 12:39:40 +0530

    Code till Advance Pattern Problem

--- src/AdvPattern.java
+++ src/AdvPattern.java
@@ -1,65 +1,84 @@
 public class AdvPattern {
-public static void inverted_rotated_half_pyramid(int n){
-    for(int i = 1; i<=n ; i++)
-    {
-        for(int j=1; j<=n-i; j++ )
-        {
-            System.out.print(" ");
-        }
-        for(int j = 1 ; j <= i ; j++ )
-        {
-            System.out.print("*");
-        }
-        System.out.println();
-    }
-}
-public static void inverted_half_pyramid(int n)
-{
-    for(int i = 1; i <= n ; i++)
-    {
-        for(int j = 1 ; j <= n-i+1 ; j++)
-        {
-            System.out.print(j);
-        }
-        System.out.println();
-    }
-}
-public static void floydsTriangle(int n) {
-    int counter=1;
-    for(int i = 1; i <= 5; i++)
-    {
-        for(int j = 1; j <= i; j++)
-        {
-            System.out.print(counter + " ");
-            counter++;
-        }
-        System.out.println();
-    }
-    
-}
-public static void triangleOI(int n)
-{
-    for(int i = 1; i <= 5; i++)
-    {
-        for(int j = 1; j <= i; j++)
-        {
-            if((i+j)%2 == 0)
-            {
-                System.out.print("1");
+    public static void inverted_rotated_half_pyramid(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
             }
-            else if((i+j)%2 != 0)
-            {
-                System.out.print("0");
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
             }
+            System.out.println();
         }
-        System.out.println();
     }
-}
+
+    public static void inverted_half_pyramid(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void floydsTriangle(int n) {
+        int counter = 1;
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(counter + " ");
+                counter++;
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void triangleOI(int n) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print("1");
+                } else if ((i + j) % 2 != 0) {
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void numPyramid(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = n; j >= i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void palindromicWithNum(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = n; j >= i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
 //         inverted_rotated_half_pyramid(6);
 //         inverted_half_pyramid(6);
-         floydsTriangle(5);
+//        floydsTriangle(5);
 //        triangleOI(5);
+//        numPyramid(5);
+        palindromicWithNum(5);
     }
-    
+
 }
