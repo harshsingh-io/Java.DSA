a0791e46d7627ec9f9d974eb8a86501ce90ef513
Harsh Singh <89487399+harshsingh-io@users.noreply.github.com>
2023-06-07 22:45:02 +0530

    Code Added In Maximum Sliding Window Problem

--- src/SlidingWindowMaximum.java
+++ src/SlidingWindowMaximum.java
@@ -1,5 +1,47 @@
-public class SlidingWindowMaximum {
-    public static void main(String[] args) {
+import java.util.PriorityQueue;
 
+public class SlidingWindowMaximum {
+    static class Pair implements Comparable<Pair> {
+        int val;
+        int idx;
+
+        public Pair(int val, int idx) {
+            this.val = val;
+            this.idx = idx;
+        }
+
+        @Override
+        public int compareTo(Pair p2) {
+            // if we have to do in ascending order
+            // return this.val-p2.val;
+            return p2.val - this.val;
+        }
     }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, -1, -3, 5, 3, 6, 7};
+        int k = 3; // windows size
+        int[] res = new int[arr.length - k + 1]; //n-k+1
+
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+
+        // 1st Window
+        for (int i = 0; i < k; i++) {
+            pq.add(new Pair(arr[i], i));
+        }
+        res[0] = pq.peek().val;
+
+        for (int i = k; i < arr.length; i++) {
+            while (pq.size() > 0 && pq.peek().idx <= (i - k)) {
+                pq.remove();
+            }
+            pq.add(new Pair(arr[i], i));
+            res[i - k + 1] = pq.peek().val;
+        }
+        //Printing Resulting Array
+        for (int i = 0; i < res.length; i++) {
+            System.out.print(res[i] + " ");
+        }
+    }
+
 }
